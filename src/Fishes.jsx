8be7cf5d7d38df858b8c4827525d9d7ad3454122
sullaspqr <sulla.spqr78@gmail.com>
2026-05-13 10:01:45 +0200

    déndzsör

--- src/Fishes.jsx
+++ src/Fishes.jsx
@@ -27,29 +27,21 @@ export const Fishes = () => {
                     <h2>Halak</h2>
                     {fishes.map((fish, index) => (
                         <div className="card col-sm-3 d-inline-block m-1 p-2" key={index}>
-                            <p className="text-dark">Sakkozó neve: {chess.name}</p>
-                            <p className="text-danger">Születési éve: {chess.birth_date}</p>
-                            <p className="text-danger">Nyert világbajnokságai: {chess.world_ch_won}</p>
+                            <p className="text-dark">Hal neve: {fish.nev}</p>
+                            <p className="text-danger">Haj faja: {fish.faj}</p>
+                            <p className="text-danger">Hal mérete: {fish.meretCm}</p>
+                            <p className="text-danger">Melyik tóban őshonos: {fish.toNev}</p>
                             <div className="card-body">
-                                {chess.profile_url.startsWith('http') ? (
-                                    <a href={chess.profile_url} target="_blank" rel="noopener noreferrer">
-                                        Profile link
-                                    </a>
-                                ) : (
-                                    <NavLink to={chess.profile_url} exact>
-                                        Profile link
-                                    </NavLink>
-                                )}
+                                
                                 <br />
-                                <NavLink to={"/chess/" + chess.id}>
-                                    <img alt={chess.nev}
-                                        className="img-fluid"
-                                        style={{ maxHeight: 200 }}
-                                        src={chess.image_url || "https://placehold.co/400x800"} />
+                                <NavLink to={"/hal/" + fish.id} className="btn btn-primary">
+                                    További adatok
+                                </NavLink>
+                                <NavLink to={"/halak"} className="btn btn-danger">
+                                    Vissza
                                 </NavLink>
                                 <br />
-                                <NavLink to={"/mod-chess/" + chess.id}><i className="bi bi-pencil"></i></NavLink> &nbsp;&nbsp;
-                                <NavLink to={"/del-chess/" + chess.id}><i className="bi bi-trash3"></i></NavLink>
+                                
                             </div>
                         </div>
                     ))}
