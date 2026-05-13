afb0568d01e0a311968b9024fdf6a254a7825a77
sullaspqr <sulla.spqr78@gmail.com>
2026-05-13 10:09:21 +0200

    Fish - kész

--- src/Fish.jsx
+++ src/Fish.jsx
@@ -6,14 +6,14 @@ export const Fish =()=> {
 
     const params = useParams();
     const id = params.Id;
-    const[chess,setChess] = useState([]);
+    const[fish,setFish] = useState([]);
     const[isPending, setPending] = useState(false);
     useEffect(() => {
         setPending(true);
         (async () => {
             try {
-        const res= await axios.get(`https://chess.sulla.hu/chess/${id}`);
-            setChess(res.data);
+        const res= await axios.get(`https://halak.sulla.hu/api/HalakDTO/${id}`);
+            setFish(res.data);
         }
         catch(error) {
             console.log(error);
@@ -27,35 +27,23 @@ export const Fish =()=> {
 
     return (
         <div className="p-5 m-auto text-center content bg-lavender">
-            {isPending || !chess.id ? (
+            {isPending || !fish.id ? (
                 <div className="spinner-border"></div>
             ) : (
                             <div className="card p-3">
                                 <div className="card-body">
-                                <h5 className="card-title">Sakkozó neve: {chess.name}</h5>
-                                <div className="lead">Születési éve: {chess.birth_date}</div>
-                                <div className="lead">Nyert világbajnokságai: {chess.world_ch_won}</div>
-                                {/* Feltételes NavLink az abszolút URL-hez */}
-{chess.profile_url.startsWith('http') ? (
-                                    <a href={chess.profile_url} target="_blank" rel="noopener noreferrer">
-                                        Profile link
-                                    </a>
-                                ) : (
-                                    <NavLink to={chess.profile_url} exact>
-                                        Profile link
-                                    </NavLink>
-                                )}
+                            <p className="text-dark">Hal neve: {fish.nev}</p>
+                            <p className="text-danger">Haj faja: {fish.faj}</p>
+                            <p className="text-danger">Hal mérete: {fish.meretCm}</p>
+                            <p className="text-danger">Melyik tóban őshonos: {fish.toNev}</p>
+                                
                                 <br />
-                                <NavLink key={chess.id} to={"/chess/" + chess.id}>
-                                    <img alt={chess.nev}
-                                        className="img-fluid"
-                                        style={{ maxHeight: 200 }}
-                                        src={chess.image_url ? chess.image_url :
-                                            "https://placehold.co/400x800"} /></NavLink>
+                                <NavLink key={fish.id} to={"/halak"}>
+                                    <button className="btn btn-primary">Vissza</button>
+                                </NavLink>
                                 <br />
                                   </div>
-                                  <div><NavLink to="/"><i className="bi bi-backspace"></i></NavLink> &nbsp;&nbsp;&nbsp;
-<NavLink key="y" to={"/mod-chess/" + chess.id}><i className="bi bi-pencil"></i></NavLink></div>   
+                                    
                             </div>
                         
                     )}
